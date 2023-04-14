b35c1fdb64beb6e23795ac8f3aed40e1e50b204b
Zakiye Zand <zakiyezand@gmail.com>
2023-04-15 01:00:47 +0430

    class student & teacher

--- HW01.py
+++ HW01.py
@@ -25,3 +25,44 @@ class Person:
     def talk(self):    # abstraction No.1
         pass
 
+class Friend:
+
+    def __init__(self, *name):  # polymorphism No.1
+        self._name = name
+
+    def greeting(self):
+        for Name in self._name:
+            print(f'Hello {Name}! how are you?')
+
+
+class Student(Person):
+
+    def __init__(self, name, age, gender, height, weight, school_name, friend_name):
+        super().__init__(name, age, gender, height, weight)  # inheritance No.1
+        self.__school_name = school_name
+        self._friend = Friend(friend_name)    # composition No.1
+
+    def study(self):
+        print(f"{self._name} is studying at {self.__school_name}.")
+
+    def Group_study(self):
+        print(f"{self._friend.greeting} \n Shall we study together?")
+
+    def talk(self):     # abstraction No.1
+        print(f"{self._name} is talking with {self._friend._name}")
+
+
+class Teacher(Person):
+
+    def __init__(self, name, age, gender, height, weight, subject):
+        super().__init__(name, age, gender, height, weight)  # inheritance No.2
+        self.__subject = subject
+
+    def teach(self):
+        print(f"{self._name} is teaching {self.__subject}.")
+
+    def talk(self):    # abstraction No.1
+        if self._gender == 'male':
+            print(f"Mr.{self._name} is talking")
+        elif self._gender == 'female':
+            print(f"Mrs.{self._name} is talking")
